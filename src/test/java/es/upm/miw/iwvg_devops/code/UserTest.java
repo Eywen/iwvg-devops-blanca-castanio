e1fa7dc0d413a36b4ad19f0ec56ff42b8bb4ab72
Blanca <b.castano@almatech.es>
2021-10-01 13:50:03 +0200

    Added UserTest #9

---
+++ src/test/java/es/upm/miw/iwvg_devops/code/UserTest.java
@@ -0,0 +1,34 @@
+package es.upm.miw.iwvg_devops.code;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class UserTest {
+    private User user;
+
+    //@BeforeEach
+    void before() {
+       // user = new User("usu001", "usuario1", "familyName", List<Fraction> fractions);
+    }
+
+    //@Test
+    void testUserStringStringStringListFraction() {
+        // TODO: to check whit FRACCION class sets into contructor of this test class
+        //assertEquals("usu001",user.getId());
+        //assertEquals("usuario1",user.getName());
+        //assertEquals("familyName",user.getFamilyName());
+        //assertEquals(new Fraction(),user.getFractions);
+    }
+
+    //@Test
+    void testUser() {
+        // TODO: uncomment when we have FRACTION class code
+        //user = new User(); //TODO: user created with constructor of parameters when we have Fraction class
+        //assertEquals("usu002",user.getId());
+        //assertEquals("usuario2",user.getName());
+        //assertEquals("familyName2",user.getFamilyName());
+        //assertEquals(new Fraction(parameters),user.getFractions);
+    }
+}
